753328e2b2b40d05e991b30847382aaa8a0715a1
Jared Winter <jwinternet@outlook.com>
2024-03-07 18:27:44 -0500

    More...

--- bookr/urls.py
+++ bookr/urls.py
@@ -1,12 +1,12 @@
+import reviews.views
 
 from django.contrib import admin
-from django.urls import path
-
-import reviews.views
+from django.urls import include, path
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", reviews.views.index),
-    path("book-search", reviews.views.book_search)
+    path('admin/', admin.site.urls),
+    path('', reviews.views.index),
+    path('book-search/', reviews.views.book_search),
+    path('', include('reviews.urls')),
 ]
